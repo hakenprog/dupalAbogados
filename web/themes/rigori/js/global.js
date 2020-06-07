cfe579d4f9a547f3ac8a87f1d5330056b7011d66
haken <hakenprog@gmail.com>
2020-06-07 14:21:02 -0500

    Nueva tema seteado

---
+++ web/themes/rigori/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.rigori = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);

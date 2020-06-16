29cbda047875577a35cca150c404d35f5c476a08
root <root@ip-172-31-40-180.sa-east-1.compute.internal>
2020-06-16 19:56:02 +0000

    Cambio de estilos

--- web/themes/rigori/js/navbar_scroll.js
+++ web/themes/rigori/js/navbar_scroll.js
@@ -1,39 +1,35 @@
-(jQuery(document).ready(function(){      
-    if(jQuery("#tituloPrincipal").length){
-    var scroll_start = 0;
-    var startchange = jQuery('#tituloPrincipal');
-    var offset = startchange.offset();
-        jQuery(document).scroll(function() { 
-              scroll_start = jQuery(window).scrollTop();
-              if(scroll_start > offset.top) {
-                  jQuery(".navbar").css({'background-color': 'darkorange', 'transition': '400ms'});
-      jQuery(".logo").css({'width':'80%'})
-              } else {
-                  jQuery('.navbar').css('background-color', 'transparent');
-      jQuery(".logo").css({'width':'100%', 'transition': '400ms'} )
-              }
-          });
-      
-   } else {
-       jQuery(".header").css('height','96px');
-       jQuery(".navbar").css({'background-color': 'darkorange'});
-     }
 
-
-  window.onhaschange = function(){
-    if(jQuery("#tituloPrincipal").length){
-      var scroll_start = 0;
-      var startchange = jQuery('#tituloPrincipal');
-          var offset = startchange.offset();
-      if(scroll_start > offset.top){
-        jQuery(".navbar").css({'background-color': 'darkorange', 'transition':'400ms'})
-                    jQuery(".logo").css({'width':'80%'})
-      }
+  window.onhashchange = function(){
+  	if (100 < jQuery(".navbar").offset().top){
+    	jQuery(".navbar").addClass("header-collapse");
+       	jQuery(".logo").css({'width':'80%','transition':'300ms'})
     }
-  }  
+  
+  }
+  
+  	jQuery(".navbar").length && jQuery(window).scroll(function() {
+    	     if (100 < jQuery(".navbar").offset().top){ 
+        	 	jQuery(".navbar").addClass("header-collapse");
+            	jQuery(".logo").css({'width':'80%','transition':'300ms'})
+         
+         	}else{
+         
+         		jQuery(".logo").css({'width':'100%', 'transition':'300ms'});
+         		jQuery(".navbar").removeClass("header-collapse");
+         
+       	}
+  	})
+  
+  
+  jQuery(document).ready(function(){
+  		if(!jQuery("#textHero").length){
+			jQuery("#header").css({'height': '86px'})
+            jQuery(".navbar").css("background-color","#A5A5A6")
+    	}    
+    })
+    
+    
   
 
-	jQuery(".navbar").length && jQuery(window).scroll(function() {
-        	200 < jQuery(".navbar").offset().top ? jQuery(".navbar").addClass("header-collapse") : jQuery(".navbar").removeClass("header-collapse")
-	})
-}))
+
+

2d06f2f82dbdf8ced160dc0a1146b32c22b37066
root <root@ip-172-31-0-164.sa-east-1.compute.internal>
2020-07-09 17:30:31 +0000

    Prueba

--- web/themes/rigori/js/navbar_scroll.js
+++ web/themes/rigori/js/navbar_scroll.js
@@ -1,33 +1,42 @@
 
-  window.onhashchange = function(){
-  	if (100 < jQuery(".navbar").offset().top){
+setTimeout(window.onload = function(){
+    if (100 < jQuery(".navbar").offset().top){ 
     	jQuery(".navbar").addClass("header-collapse");
-       	jQuery(".logo").css({'width':'80%','transition':'300ms'})
+        jQuery(".logo").css({'width':'80%','transition':'300ms'})    
     }
+    
+   	if(!jQuery("#textHero").length){
+			var height = document.getElementById('navbar-main').offsetHeight
+            jQuery("#main-wrapper").css('margin-top', height);
+            jQuery(".navbar").addClass("header-collapse");
+  	}      
+    
+    
+  }, 700);
+
   
-  }
-  
-  	jQuery(".navbar").length && jQuery(window).scroll(function() {
-    	     if (100 < jQuery(".navbar").offset().top){ 
+   if(jQuery("#textHero").length){ 
+    jQuery(".navbar").length && jQuery(window).scroll(function() {
+    	if (100 < jQuery(".navbar").offset().top){ 
         	 	jQuery(".navbar").addClass("header-collapse");
             	jQuery(".logo").css({'width':'80%','transition':'300ms'})
-         
-         	}else{
+        
+       	}
+        
+        else{
          
          		jQuery(".logo").css({'width':'100%', 'transition':'300ms'});
          		jQuery(".navbar").removeClass("header-collapse");
          
        	}
   	})
-  
-  
-  jQuery(document).ready(function(){
-  		if(!jQuery("#textHero").length){
-			jQuery("#header").css({'height': '86px'})
-            jQuery(".navbar").css("background-color","#A5A5A6")
-    	}    
-    })
+    }
     
+    
+ 
+  
+  
+  
     
   
 
